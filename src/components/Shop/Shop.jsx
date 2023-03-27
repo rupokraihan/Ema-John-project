17e159d3926c1cd45f6afe4ce294a4168d926c81
rupokraihan <rupok2321@gmail.com>
2023-03-28 03:21:04 +0600

    add to cart complete

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,25 +1,35 @@
-import React, { useEffect, useState } from 'react';
-import './Shop.css'
+import React, { useEffect, useState } from "react";
+import Product from "../product/Product";
+import "./Shop.css";
 const Shop = () => {
-
   const [products, setProducts] = useState([]);
+  const [cart, setCart] =useState([])
 
-  useEffect(() =>{
-    fetch('products.json')
-    .then(res => res.json())
-    .then(data => setProducts(data))
+  useEffect(() => {
+    fetch("products.json")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
 
+  const handleAddToCart = (product) => {
+    const newCart = [...cart, product];
+    setCart(newCart);
+  };
 
-  }, [])
   return (
-    <div className='shop-container'>
+    <div className="shop-container">
       <div className="products-container">
-        <h1>hello products : {products.length }</h1>
-
+        {products.map((product) => (
+          <Product
+            key={product.id}
+            product={product}
+            handleAddToCart={handleAddToCart}
+          ></Product>
+        ))}
       </div>
       <div className="cart-container">
-        <h3>summary</h3>
-
+        <h3>Order summary</h3>
+        <p>Selected Items: {cart.length}</p>
       </div>
     </div>
   );
